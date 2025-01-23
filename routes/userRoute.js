01ec2784f99e1f7791b91de86f9dfbb27512114c
Ayush Kaji Dangol <ayushdangol47@gmail.com>
2025-01-23 12:18:01 +0545

    add auth to the backend

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -5,10 +5,14 @@ const router = express.Router();
 
 const userController = require('../controllers/userController')
 
-router.get('/view_users',userController.getUser)
-router.post('/create_users',userController.createUser)
 
-router.put('/:id',userController.updateUser)
-router.delete('/:id',userController.deleteUser)
+router.post('/login', userController.loginUser);
+router.post('/signup', userController.registerUser);
+
+// router.get('/view_users',userController.getUser)
+// router.post('/create_users',userController.createUser)
+
+// router.put('/:id',userController.updateUser)
+// router.delete('/:id',userController.deleteUser)
 
 module.exports = router;
